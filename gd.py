0c1eb835f8e58e31fabd82259f8e6555d45670d9
Polaricito <polaricito@aol.com>
2026-06-24 15:46:28 -0500

    Adds the script

---
+++ gd.py
@@ -0,0 +1,16 @@
+#super premium gd unlocker (not buys the game) 
+a = int(input('Enter 1st N: '))
+b = int(input('Enter 2nd N: '))
+c = int(input('Enter 3rd N: '))
+d = int(input('Enter 4th N: '))
+e = int(input('Enter 5th N: '))
+f = int(input('Enter 6th N: '))
+
+n1 = str(b - a)
+n2 = str(c - b)
+n3 = str(d - c)
+n4 = str(e - d)
+n5 = str(f - e)
+
+
+print('The code is:', n1 + n2 + n3 + n4 + n5, '\n')
